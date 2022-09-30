77c72396dcefec0cb4caf5cef033203b7abc69c1
Mukhammadjon <93770723+Muhammadjon-is@users.noreply.github.com>
2022-09-30 15:04:04 +0200

    Add files via upload

---
+++ Portfolio/script.js
@@ -0,0 +1,19 @@
+ function Home(){
+    alert("It is in progress 😇")
+ }
+
+function About(){
+    alert("My name is Muhammadjon, I'm 21 years old 😎  ")
+}
+
+function Skills(){
+    alert("Skills:nmap, metasploit, nikto, burpsuite, sqlmap ")
+}
+function Education(){
+    alert("Self-taught")
+}
+function Entire(){
+    alert("Welcome to my web  page ")
+    // confirm("Are you human?")
+}
+
